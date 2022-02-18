66e63d47342fe1f0a35a4c22b2d3f2ce209c358d
TejShind <tej.gholap25@gmail.com>
2022-02-18 19:26:26 +0530

    [Tejaswini]ADDED:Feature for Calculator

---
+++ src/com/blz/Calculator.java
@@ -0,0 +1,12 @@
+package com.blz;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String args[]) {
+
+        System.out.println("Welcome to Calculator");
+    }
+}
+
