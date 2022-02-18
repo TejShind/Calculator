f84be8ba13110a1044a3176060bcd26aa53d63e1
TejShind <tej.gholap25@gmail.com>
2022-02-18 19:42:38 +0530

    [Tejaswini]ADDED:Feature for Addition of two numbers

--- src/com/blz/Calculator.java
+++ src/com/blz/Calculator.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class Calculator {
 
     public static void main(String args[]) {
+        Scanner sc = new Scanner ( System.in);
+        System.out.println ("Please enter the number");
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+        int c = a + b ;
 
-        System.out.println("Welcome to Calculator");
+        System.out.println("The sum of numbers is: " + c);
     }
 }
 
